50b44282c165f593ca92421e158434981a17183e
Xinjie <mrwangxinjie@163.com>
2026-07-08 01:03:43 +0800

    chore(docs): Update video link. (#78)
    * chore(docs): Update video link.
    
    * chore(docs): Update video link.
    
    * chore(docs): Update video link.

--- apps/visualize_asset.py
+++ apps/visualize_asset.py
@@ -824,7 +824,7 @@ with gr.Blocks(
             <a href="https://github.com/HorizonRobotics/EmbodiedGen">
                 <img alt="💻 GitHub" src="https://img.shields.io/badge/GitHub-000000?logo=github">
             </a>
-            <a href="https://youtu.be/DVRhSR8ukmE">
+            <a href="https://youtu.be/MIkJJSVM8L4">
                 <img alt="🎥 Video" src="https://img.shields.io/badge/🎥-Video-red">
             </a>
         </p>
